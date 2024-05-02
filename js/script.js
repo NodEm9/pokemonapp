09a549ee549c6699b091112a07e18fdec0e9cbec
Emmanuel Nodolomwanyi <emma0martins@gmail.com>
2024-05-02 19:08:15 +0200

    Put the pokemonList variable inside an IIFE function to make it a protected global variable that can be accessed only via it's return function objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-//Pokemom list array
+// 
+
+let pokemonRepository = (function () {
+  //Pokemom list array
 let pokemonList = [
   { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
   { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
@@ -9,25 +12,62 @@ let pokemonList = [
   { name: "Squirtle", height: 0.5, types: ["water"] },
   { name: "Wartortle", height: 1, types: ["water"] },
   { name: "Blastoise", height: 1.5, types: ["water"] }
-];
+  ];
+  
+  //Function to add new pokemon to the list
+  function add(pokemon) {
+    if (typeof pokemon === "object") {
+      pokemonList.push(pokemon);
+    } else {
+      console.log("You need to add a Pokemon object");
+    }
+  }
 
-//Create h1 element with text "Pokémon List" 
-document.write("<h1>Pokémon List</h1>");
+  //Function to get all pokemon from the list
+  function getAll() {
+    return pokemonList;
+  }
+
+  //Function to show details of the pokemon
+   //Function to show details of each Pokémon
+  function showDetails(pokemon) {
+    console.log(pokemon);
+    let pokemonDetails = document.querySelector(".pokemon-details");
+    pokemonDetails.innerHTML = pokemon.name + " (height: " + pokemon.height + ")";
+    return pokemon;
+  };
+
+    //Function to add list item to the DOM
+    function addListItem(pokemon) {
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listPokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      listPokemon.appendChild(button);
+      pokemonList.appendChild(listPokemon);
+      button.addEventListener("click", function(event) {
+        showDetails(pokemon);
+      });
+    }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+
+})();
+
+// //Create h1 element with text "Pokémon List" 
+// document.write("<h1>Pokémon List</h1>");
 
 /* Create unordered list element and use  forEach loop 
 * to display Pokémon name and height in list items.
 * Add conditional to display message if height is greater than 1.5
 */
-document.write("<ul>");
-pokemonList.forEach(function (pokemon) { 
-  document.write("<li>" + pokemon.name + " (height: " + pokemon.height + ")");
-  if (pokemon.height > 1.5) {
-    document.write(" - Wow, that's big!");
-  }
-  document.write("</li>");
+pokemonRepository.getAll().forEach(function (pokemon) { 
+  pokemonRepository.addListItem(pokemon);
 });
-document.write("</ul>");
-
-console.log(pokemonList);
 
 
